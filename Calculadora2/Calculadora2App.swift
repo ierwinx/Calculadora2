8beb93ca09b4c7d9ba91e99c5dc3337d4f0da545
Eяw!и Gontier <ierwinx@me.com>
2022-09-27 22:07:03 -0500

    Initial Commit

---
+++ Calculadora2/Calculadora2App.swift
@@ -0,0 +1,17 @@
+//
+//  Calculadora2App.swift
+//  Calculadora2
+//
+//  Created by Erwin Luz León on 27/09/22.
+//
+
+import SwiftUI
+
+@main
+struct Calculadora2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
